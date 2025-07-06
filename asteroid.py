776a30bffd499984228495d3bb82b5cd05498955
bladen <thealmightypsycho@gmail.com>
2025-07-06 00:43:26 -0400

    add shots destroy or split asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from constants import *
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -10,3 +12,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        asteroid_a = self.velocity.rotate(random_angle)
+        asteroid_b = self.velocity.rotate(-random_angle)
+        asteriod_size = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, asteriod_size)
+        asteroid.velocity = asteroid_a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, asteriod_size)
+        asteroid.velocity = asteroid_b * 1.2
--- main.py
+++ main.py
@@ -33,6 +33,11 @@ def main():
                 print("Game Over!")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
